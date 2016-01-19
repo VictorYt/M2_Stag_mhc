74e992d74f939f27afa0926a681b13eed21320f5
VictorYt <victor.ythier@gmail.com>
2016-01-19 17:26:44 +0100

    qblast plus rapide reste a vérifier pour .3 et récupérer les positions

--- SNPpos.py
+++ SNPpos.py
@@ -2,9 +2,10 @@
 
 import csv 
 import sys
-from Bio import pairwise2
+import time
+from Bio import pairwise2 #pas utilise
 from Bio.Blast import NCBIWWW
-from Bio.Blast import NCBIXML
+from Bio.Blast import NCBIXML #pas encore ultilise
 
 
 #Variables
@@ -30,6 +31,7 @@ with open(sys.argv[1],"rb") as csvfileEntree:
 ##########Obtention de la position du SNP dans genome de ref (galGal4 puis galGal5)
 ######################
 
+#A faire
 #regarder comment prendre qu'une partie de la sequence. av [.../...]
 #regarder pour faire la meme chose avec les 2 sequences de references NC_006103.3 et .4
 #regarder si on peut avoir la taille de la querry a la sortie
@@ -38,6 +40,64 @@ with open(sys.argv[1],"rb") as csvfileEntree:
 #ajout de commentaire auto
 #ajout de la position dans le dico en fonction de la querry utilisee
 #creer les fichiers de sorties
+
+
+
+#Boucler avec les 96 SNP
+	#faire une fonction pour selectionner juste la seq flanquante gauche ou droite
+
+#trop long (11min/seq) mais marche avec la derniere version dassemblage
+#fichier de sortie de l alignement
+#reste a choisir 
+	#le bon alignement
+	#la position du SNP en fonction de la sequence flanquante choisie
+print "lance blast :"
+debut = time.time()
+result_handle = NCBIWWW.qblast("blastn", "refseq_genomic",sequence="TTGGGTGTTTTGACCCAAAACCTATTCATGGTGAAGACACCTTTGGAGGAACTGATGCAGTCGGGTGGGGACACACAGAGGGATTCACCCCACATTTCCC", entrez_query="NC_006103.3[RefSeq]", format_type='Text')
+#(plus lente)result_handle = NCBIWWW.qblast("blastn", "refseq_genomic",sequence="TTGGGTGTTTTGACCCAAAACCTATTCATGGTGAAGACACCTTTGGAGGAACTGATGCAGTCGGGTGGGGACACACAGAGGGATTCACCCCACATTTCCC", entrez_query="GCF_000002315.4[assembly]", format_type='Text')
+print "blast fini \t"
+fin = time.time()
+print "il a mis :", fin-debut
+save_file = open("my_blast", "w")
+print "write in .txt file"
+save_file.write(result_handle.read())
+save_file.close()
+
+""" c est nul ce truc/marche pas
+result_handle.close()
+result_handle = open("my_blast.xml")
+records = NCBIXML.parse(result_handle)
+blast_record = records.next()
+for alignment in blast_record.alignments:
+	for hsp in alignment.hsps:
+		if hsp.expect <0.01:
+			print ('*****Alignement*****')
+			print "sequence:"alignment.title
+			print "length:"alignment.length
+			print "e value:"hsp.expect
+			print "strand :"hsp.strand
+			print "position : "
+			print (hsp.query[0:len(hsp.query)] + '....')
+			print (hsp.match[0:len(hsp.query)] + '....')
+			print (hsp.sbjst[0:len(hsp.query)] + '....')
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 """
@@ -60,43 +120,12 @@ for seq_SNP in  dicoSNP.values():
 		#for a in alignments:
 		#print format_alignnment(*a)
 
-
-"""
-"""
-#Avec la commande qblast de biopython
-result_handle = NCBIWWW.qblast("blastn", "nt", "TTGGGTGTTTTGACCCAAAACCTATTCATGGTGAAGACACCTTTGGAGGAACTGATGCAGTCGGGTGGGGACACACAGAGGGATTCACCCCACATTTCCC", format_type="Text")
-save_file = open("my_blast.xml", "w")
-save_file.write(result_handle.read())
-save_file.close()
-result_handle.close()
-result_handle = open("my_blast.xml")
-records = NCBIXML.parse(result_handle)
-blast_record = records.next()
-for alignment in blast_record.alignments:
-	for hsp in alignment.hsps:
-		if hsp.expect <0.01:
-			print ('*****Alignement*****')
-			print ('sequence:'alignment.title)
-			print ('length:'alignment.length)
-			print ('gaps:'hsp.score)
-			print ('e value:'hsp.expect)
-			print ('strand :'hsp.strand)
-			print (hsp.query[0:len(hsp.query)] + '....')
-			print (hsp.match[0:len(hsp.query)] + '....')
-			print (hsp.sbjst[0:len(hsp.query)] + '....')
-#Pb = Possible mais commande supplementaire pour choisir l'alignement avec les sequences de references que je souhaite CM000108.3 et.4
+#Ce n'est pas la solution
 """
 
-#Marche mais ne me donne pas d'alignement avec la sequence de ref que je souhaite
-#boucler avec les 96 SNP
-#faire une fonction pour selectionner juste la seq flanquante gauche ou droite
-#voir quelle database utiliser
-#reussir a lui faire aligner sur les sequence de ref NC_006103 (.3 et .4)
-#long mais marche avec la dernière version dassemblage
-result_handle = NCBIWWW.qblast("blastn", "refseq_genomic",sequence="TTGGGTGTTTTGACCCAAAACCTATTCATGGTGAAGACACCTTTGGAGGAACTGATGCAGTCGGGTGGGGACACACAGAGGGATTCACCCCACATTTCCC", entrez_query="GCF_000002315.4[assembly]", format_type='Text')
-save_file = open("my_blast", "w")
-save_file.write(result_handle.read())
-save_file.close()
+
+
+
 
 
 ######################
@@ -108,3 +137,12 @@ with open(argv[3], 'wb') as csvfileSortie:
 	crS = csv.writer(csvfileSortie, delimiter='\t', quotechar='|', quoting=csv.QUOTE_MINIMAL)
 	crS.writerow()
 """
+
+
+
+
+
+
+
+
+
