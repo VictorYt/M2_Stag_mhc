51b7b3e37681e2669346b53ab581475344ee4548
VictorYt <victor.ythier@gmail.com>
2016-01-29 16:42:44 +0100

    Just need to customise getMultiFasta file

--- 2nd_try/getMultiFasta.py
+++ 2nd_try/getMultiFasta.py
@@ -2,53 +2,55 @@
 # -*- coding: utf-8 -*-
 
 
-"""Transforamtion d'un fichier text (.csv, sep={tabulation}) en fichier multi-fasta
-Voir http://biopython.org/wiki/SeqIO#Sequence_Output
-Pour savoir qu'elle format d'input est géré par Bio.SeqIO
-
-
-Attention le fichier de sortie contient, en plus des deux séquences flanquantes le ploymorphisme entre corchet [A/B]"""
-
 from sys import argv
 from csv import reader, writer
 from Bio import SeqIO
 
+#pour faire ce que je veux http://sequenceconversion.bugaco.com/converter/biology/sequences/tab_to_fasta.php
+#mais pas de découpage des seq flanquante
+#pour faire l'inverse de ce que je veux http://darwin.biochem.okstate.edu/fasta2tab/
 
- 
- 
-def fonction(source, outp, delimit= "\t"):
-	"""Transformation du fichier tabulé en iput vers un fichier multi-fasta """
-	with open(source, 'rb') as src, open(outp, 'wb') as otp:
+"""Programme permettant de passer d'un format texte (.csv) à 2 colonnes (SNP_ID/Sequences)
+à un multi-format fasta qui contient pour chaque SNP les 2 séquences flanquantes seules"""
+
+
+def getSeqFlanq5(seq):
+	"""n la séquence avec les 2 sequences flanquantes et le polymorphuisme [A/B]
+	getSeqFlanq5 renvoie le string avant le caractère '[' """
+	return seq.rsplit("[", 1)[0]
+
+def getSeqFlanq3(seq):
+	"""seq la séquence avec les 2 sequences flanquantes et le polymorphuisme [A/B]
+	getSeqFlanq5 renvoie le string après le caractère '[' """
+	return seq.rsplit("]", 1)[1]
+
+
+def TransformeInput(inpt, outp, delimit = "\t"):
+	"""Transformation du fichier tabulé en un autre fichier tabulé dans lequel
+	la sequence est coupé par sequences flanquantes"""
+	with open(inpt, 'rb') as src, open(outp, 'wb') as otp:
 		my_reader = reader(src, delimiter = delimit)
-		#pas de csv en sortie mais un .fasta pas ça
-		my_writer = writer(otp, delimiter = "\n")
-		liste = list()
+		my_writer = writer(otp, delimiter = "\t")
+		for rows in my_reader :
+			my_writer.writerow(("%s 5'" %(rows[0]), getSeqFlanq5(rows[1])))
+			my_writer.writerow(("%s 3'" %(rows[0]), getSeqFlanq3(rows[1])))
 
-		for column in zip(*my_reader):
-			n = 10
-			liste.append(n)
-		my_writer.writerows(liste)
-
-		#plutôt comme ça en utlisant biopython
-		sequence = #change alternativement entre SNP seqflanq5/seqflanq3
-		my_writer = SeqIO.write(sequence, otp, "fasta")
-
-
-def getSeqFlanq5():
-	"""Permet de récupérer la séquence flanquante de gauche avant le polymorphisme marqué par [A/B]"""
-	pass
-
-def getSeqFlanq3():
-	"""Permet de récupérer la séquence flanquante de droite après le polymorphisme marqué par [A/B]"""
-	pass
 
 
 
 if __name__ == '__main__':
+	""" tabulation delimiteur par defaut mais possibilité de le spécifier si ce n'est pas le cas"""
 	if len(argv) == 3:
-		fonction(argv[1], argv[2])
+		TransformeInput(argv[1],argv[2])
+		RealTransforme = SeqIO.convert(argv[2], "tab", "multi_maker.fasta","fasta")
 	elif len(argv) == 4:
-		fonction(argv[1], argv[2], argv[3])
+		TransformeInput(argv[1],argv[2], argv[3])
+		RealTransforme = SeqIO.convert(argv[2], "tab", "multi_maker.fasta","fasta")
 	else:
 		print("C\'est faux")
 	print("Terminé")
+
+
+#custumisation
+	#gérer les input (pas normal que l'on donne le nom de l'intermédiaire et pas le final)
+	#gérer l'odre des arguments (styme -i -o -d -f(lui trouver une lettre qui convient))
