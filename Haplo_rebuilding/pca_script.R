452b35a186142e05166fe9b4bf0169e71ea1dc9c
VictorYt <victor.ythier@gmail.com>
2016-04-08 12:05:07 +0200

    script almost finished, need to see with bertrand about missing data, implement it in pyhtonscript now

--- Haplo_rebuilding/pca_script.R
+++ Haplo_rebuilding/pca_script.R
@@ -113,14 +113,16 @@ all_haplo.genind<-df2genind(all_haplo, sep = "", pop = factor(origin.all))
 
 
 #4st step : Missing data (graphics and replacing missing data by the average of the SNP)
-#(ici aussi gérer les stdouts)
-#(voir pour faire des graphe ggplot2)
+#(ici aussi gérer les stdouts et dotchart)
+#(voir pour faire des graphe ggplot2 / faire une echelle horizontale de couleur)
+
 #Haplotypes we already Know#
 fic<-tab(haplo_hmz.genind)
 dim(fic)
 fic1<-fic[,seq(1, ncol(fic),2)]
 dim(fic1)
 fic1->fic
+fic1->fic.known
 rm(fic1)
 tabmiss.known<-as.data.frame(1*is.na(fic))
   #number of missing values per column: colmiss/ row: rowmiss
@@ -133,7 +135,26 @@ table(rowmiss.known)
 dotchart(colmiss.known, label="")
 dotchart(rowmiss.known, label="")
 
-
+#replace Missing Data.
+#No missing data in our known haplotypes (of course), so i can take the initial tab
+fic2<-as.data.frame(fic.known)#non missing data i use fic.known
+fic2i<-fic2
+fic2i[,1:length(fic2)]<-impute(fic2[,1:length(fic2)],what="mean")
+fic2i<-fic2i/2
+#Total frequency
+pro<-apply(fic2i, 2, mean)
+#Standard deviation
+std<-sqrt (pro*(1-pro) )
+#Reduced centered matrix
+xystd.known<-scale(fic2i, center = TRUE, scale = std) 
+identif<-rownames(xystd.known)
+iden.known<-strsplit(identif, ":")
+iden.known<-unlist(iden.known)
+origin.known<-iden.known[seq(1,length(iden.known),2)]
+IDs_name.known<-iden.known[seq(2,length(iden.known),2)]
+popg.known<-factor(origin.known)
+popg.known<-factor(origin.known, exclude = NULL)
+table(popg.known)
 
 #Candidats Haplotypes#
 fic<-tab(new_haplo.genind)
@@ -141,6 +162,7 @@ dim(fic)
 fic1<-fic[,seq(1, ncol(fic),2)]
 dim(fic1)
 fic1->fic
+fic1->fic.new
 rm(fic1)
 tabmiss.new<-as.data.frame(1*is.na(fic))
 #number of missing values per column: colmiss/ row: rowmiss
@@ -153,12 +175,40 @@ table(rowmiss.new)
 dotchart(colmiss.new, label="")
 dotchart(rowmiss.new, label="", color = "blue")
 
+#replace missing data by the average value of SNP (if thresold >0).
+#(quel seuil je prend ici ? >0 pour avoir les haplotypes sans valeur manquante ou plus?)
+#a priori je prendrai sans une seul missing data car c'est ce qui nous interesse ici
+anim0.new<-rowmiss.new[rowmiss.new>0]
+fic1.new <- fic.new[-c(which(rownames(fic.new) %in% names(anim0.new)) ), ]
+
+
+fic2<-as.data.frame(fic1.new)
+fic2i<-fic2
+fic2i[,1:length(fic2)]<-impute(fic2[,1:length(fic2)],what="mean")
+fic2i<-fic2i/2
+#Total frequency
+pro<-apply(fic2i, 2, mean)
+#Standard deviation
+std<-sqrt (pro*(1-pro) )
+#Reduced centered matrix
+xystd.new<-scale(fic2i, center = TRUE, scale = std) 
+identif<-rownames(xystd.new)
+iden.new<-strsplit(identif, ":")
+iden.new<-unlist(iden.new)
+origin.new<-iden.new[seq(1,length(iden.new),2)]
+IDs_name.new<-iden.new[seq(2,length(iden.new),2)]
+popg.new<-factor(origin.new)
+popg.new<-factor(origin.new, exclude = NULL)
+table(popg.new)
+
+
 #All haplotypes data#
 fic<-tab(all_haplo.genind)
 dim(fic)
 fic1<-fic[,seq(1, ncol(fic),2)]
 dim(fic1)
 fic1->fic
+fic1->fic.all
 rm(fic1)
 tabmiss.all<-as.data.frame(1*is.na(fic))
 #number of missing values per column: colmiss/ row: rowmiss
@@ -171,20 +221,81 @@ table(rowmiss.all)
 dotchart(colmiss.all, label="")
 dotchart(rowmiss.all, label="")
 
+#replace missing data by the average value of SNP (if thresold >0).
+#(quel seuil je prend ici ? >0 pour avoir les haplotypes sans valeur manquante ou plus?)
+#a priori je prendrai sans une seul missing data car c'est ce qui nous interesse ici
+anim0.all<-rowmiss.all[rowmiss.all>0]
+fic1.all <- fic.all[-c(which(rownames(fic.all) %in% names(anim0.all)) ), ]
+
+
+fic2<-as.data.frame(fic1.all)
+fic2i<-fic2
+fic2i[,1:length(fic2)]<-impute(fic2[,1:length(fic2)],what="mean")
+fic2i<-fic2i/2
+#Total frequency
+pro<-apply(fic2i, 2, mean)
+#Standard deviation
+std<-sqrt (pro*(1-pro) )
+#Reduced centered matrix
+xystd.all<-scale(fic2i, center = TRUE, scale = std) 
+identif<-rownames(xystd.all)
+iden.all<-strsplit(identif, ":")
+iden.all<-unlist(iden.all)
+origin.all<-iden.all[seq(1,length(iden.all),2)]
+IDs_name.all<-iden.all[seq(2,length(iden.all),2)]
+popg.all<-factor(origin.all)
+popg.all<-factor(origin.all, exclude = NULL)
+table(popg.all)
 
 
 
 #Last step : Principal components analysis
-#(xystd change 3 fois faire une fonction avec le xystd qui change)
-xy.pca<-dudi.pca(xystd, scale=FALSE, scannf=FALSE, nf = nrow(xystd)-1) #ACP - on retient tous les axes
+
+#Haplotypes we already Know#
+pdf("Known_haplotypes_pca.pdf", height = 10, width = 10)
+xy.known.pca<-dudi.pca(xystd.known, scale=FALSE, scannf=FALSE, nf = nrow(xystd.known)-1) #ACP - on retient tous les axes
 par(mfrow=c(2,2))
-barplot(xy.pca$eig)
-s.class(xy.pca$li, fac = as.factor(popg), col = rep(1:5,3),xax = 1, yax = 2)
+barplot(xy.known.pca$eig)
+s.class(xy.known.pca$li, fac = as.factor(popg.known), col = rep(1:5,3),xax = 1, yax = 2)
 title(sub = "Axes 1 & 2")
-s.class(xy.pca$li, fac = as.factor(popg), col = rep(1:5,3),xax = 1, yax = 3)
+s.class(xy.known.pca$li, fac = as.factor(popg.known), col = rep(1:5,3),xax = 1, yax = 3)
 title(sub = "Axes 1 & 3")
-s.class(xy.pca$li, fac = as.factor(popg), col = rep(1:5,3),xax = 2, yax = 3)
+s.class(xy.known.pca$li, fac = as.factor(popg.known), col = rep(1:5,3),xax = 2, yax = 3)
 title(sub = "Axes 2 & 3")
 mtext("ACP sur haplotype normalisés", outer = TRUE, line = -2)
-acpeig<-100*round(xy.pca$eig/sum(xy.pca$eig),2)
+acpeig.known<-100*round(xy.known.pca$eig/sum(xy.known.pca$eig),2)
 par(mfrow=c(1,1))
+dev.off()
+
+#Candidats Haplotypes#
+#(xystd change 3 fois faire une fonction avec le xystd qui change)
+pdf("Candidates_haplotypes_pca.pdf", height = 10, width = 10)
+xy.new.pca<-dudi.pca(xystd.new, scale=FALSE, scannf=FALSE, nf = nrow(xystd.new)-1) #ACP - on retient tous les axes
+par(mfrow=c(2,2))
+barplot(xy.new.pca$eig)
+s.class(xy.new.pca$li, fac = as.factor(popg.new), col = rep(1:5,3),xax = 1, yax = 2)
+title(sub = "Axes 1 & 2")
+s.class(xy.new.pca$li, fac = as.factor(popg.new), col = rep(1:5,3),xax = 1, yax = 3)
+title(sub = "Axes 1 & 3")
+s.class(xy.new.pca$li, fac = as.factor(popg.new), col = rep(1:5,3),xax = 2, yax = 3)
+title(sub = "Axes 2 & 3")
+mtext("ACP sur haplotype normalisés", outer = TRUE, line = -2)
+acpeig.new<-100*round(xy.new.pca$eig/sum(xy.new.pca$eig),2)
+par(mfrow=c(1,1))
+dev.off()
+
+#All haplotypes data#
+pdf("All_haplotypes_pca.pdf", height = 10, width = 10)
+xy.all.pca<-dudi.pca(xystd.all, scale=FALSE, scannf=FALSE, nf = nrow(xystd.all)-1) #ACP - on retient tous les axes
+par(mfrow=c(2,2))
+barplot(xy.all.pca$eig)
+s.class(xy.all.pca$li, fac = as.factor(popg.all), col = rep(1:5,3),xax = 1, yax = 2)
+title(sub = "Axes 1 & 2")
+s.class(xy.all.pca$li, fac = as.factor(popg.all), col = rep(1:5,3),xax = 1, yax = 3)
+title(sub = "Axes 1 & 3")
+s.class(xy.all.pca$li, fac = as.factor(popg.all), col = rep(1:5,3),xax = 2, yax = 3)
+title(sub = "Axes 2 & 3")
+mtext("ACP sur haplotype normalisés", outer = TRUE, line = -2)
+acpeig.all<-100*round(xy.all.pca$eig/sum(xy.all.pca$eig),2)
+par(mfrow=c(1,1))
+dev.off()
