f47b36cb602160597373f36ab3ca0c858a5877c8
VictorYt <victor.ythier@gmail.com>
2016-04-14 15:14:17 +0200

    accesseur haplotype class ok

--- Haplo_compare/Scripts/Haplotype.py
+++ Haplo_compare/Scripts/Haplotype.py
@@ -4,72 +4,101 @@
 #Haplotype mother class
 
 class Haplotype(object):
-		def __init__(self, name, sequence, markers):
-			"""Class constructor Haplotype
+	def __init__(self, name, sequence, markers):
+		"""Class constructor Haplotype
 
-			Haplotype Class is characterized by :
-				-A name, 
-				-a sequence
+		Haplotype Class is characterized by :
+			-A name, 
+			-a sequence
 
-			"""
-			self._name = name 
-			self._sequence = sequence
-			self._markers = markers
+		"""
+		self._name = name 
+		self._sequence = sequence
+		self._markers = markers
+		self._half_geno_compatibility = []
+		self._number_of_half_copatibility = 0
+		self._entire_geno_compatibility = []
+		self._number_of_entire_copatibility = 0
 
-		############
-		#ACCESSEURS#
-		############
 
-		def _get_name(self):
-			"""Return the attribut name of the Haplotype class"""
-			return self._name
 
-		def _get_sequence(self):
-			"""Return the attribut sequence of the Haplotype class"""
-			return self._sequence
+	############
+	#ACCESSEURS#
+	############
 
-		def _get_markers(self):
-			"""Return the attribut markers of the Haplotype class which is
-			a list of the markers names used for genotyping 
+	def _get_name(self):
+		"""Return the attribut name of the Haplotype class"""
+		return self._name
+
+	def _get_sequence(self):
+		"""Return the attribut sequence of the Haplotype class"""
+		return self._sequence
+
+	def _get_markers(self):
+		"""Return the attribut markers of the Haplotype class which is
+		a list of the markers names used for genotyping 
+		
+		"""
+		return self._markers
+
+	def _get_half_geno_compatibility(self):
+		"""Return a list of genotype instace with which our haplotype is compatible"""
+		return self._half_geno_compatibility
+
+	def _get_number_of_half_compatibility(self):
+		"""Return the number of time our halpotype is compatible with the half genotype sequence.
+		This number is the length of the list with the same name.
+
+		"""
+		return self._number_of_half_copatibility
+
+	def _get_entire_geno_compatibility(self):
+		"""Return a list of genotype instace with which our haplotype and a other confirme the entire sequence"""
+		return self._entire_geno_compatibility
+
+	def _get_number_of_entire_compatibility(self):
+		"""Return the number of time our halpotype and another are compatible with the entire genotype sequence.
+		This number is the length of the list with the same name.
+
+		"""
+		return self._number_of_entire_copatibility
+
+
+	###########
+	#MUTATEURS#
+	###########
+
+	def _set_name (self, newname):
+		"""Change the name of our Haplotype object by a new one
+
+		Named parameters :
+		newname -- the new name selected (here first row of the input)
 			
-			"""
-			return self._markers
+		"""
+		self._name = newname
 
-		###########
-		#MUTATEURS#
-		###########
+	def _set_sequence(self, newsequence):
+		"""Change the sequence list of our Haplotype object by a new one
 
-		def _set_name (self, newname):
-			"""Change the name of our Haplotype object by a new one
-
-			Named parameters :
-			newname -- the new name selected (here first row of the input)
+		Named parameters :
+		newsequence -- the new sequence list selected (here the end of the row in our input)
 			
-			"""
-			self._name = newname
+		"""
+		self._sequence = sequence
 
-		def _set_sequence(self, newsequence):
-			"""Change the sequence list of our Haplotype object by a new one
+	def _set_markers(self, lstmarkers):
+		"""Change the markers list of our Haplotype object by a new one
 
-			Named parameters :
-			newsequence -- the new sequence list selected (here the end of the row in our input)
+		Named parameters :
+		lstmarkers -- the new markers list selected (here the header of the input)
 			
-			"""
-			self._sequence = sequence
+		"""
+		self._markers = lstmarkers 
 
-		def _set_markers(self, lstmarkers):
-			"""Change the markers list of our Haplotype object by a new one
+	############
+	#PROPERTIES#
+	############
 
-			Named parameters :
-			lstmarkers -- the new markers list selected (here the header of the input)
-			
-			"""
-			self._markers = lstmarkers 
-
-		############
-		#PROPERTIES#
-		############
-
-		name = property(_get_name, _set_name)
-		sequence = property(_get_sequence, _set_sequence)
-		markers = property(_get_markers, _set_markers)
+	name = property(_get_name, _set_name)
+	sequence = property(_get_sequence, _set_sequence)
+	markers = property(_get_markers, _set_markers)
