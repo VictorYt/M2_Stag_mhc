b4d348645eb00a3f90ee286c7b737ee37c7aec58
VictorYt <victor.ythier@gmail.com>
2016-05-10 13:59:26 +0200

    convertiseur ped format done

--- convertiseur_ped.py
+++ convertiseur_ped.py
@@ -21,6 +21,7 @@ if __name__ == "__main__":
     from docopt import docopt
     import os
     import csv
+    import itertools as it #used fonction chain & islice
  
     # Parse arguments, use file docstring as a parameter definition
     arguments = docopt(__doc__, version='Haplotype rebuilding 0.3')
@@ -37,54 +38,85 @@ if __name__ == "__main__":
 
 
 
-def header_line_maker(map_f):
-    """Return the header line list (empty first value and markers list)"""
-    header = []
-    header.append("")
+    def header_line_maker(map_f):
+        """Return the header line list (empty first value and markers list)"""
+        header = []
+        header.append("")
 
-    with open(map_f, 'r') as my_map :
-        my_map_reader = csv.reader(my_map, delimiter="\t")
+        with open(map_f, 'r') as my_map :
+            my_map_reader = csv.reader(my_map, delimiter="\t")
 
-        for rows in my_map_reader :
-            header.append(rows[1])
+            for rows in my_map_reader :
+                header.append(rows[1]) #rows[1] is the SNP name, rows[3] if we want SNP position
 
-    return header
-
-
-def line_maker(seq):
-    """Return a list of allele format we want"""
-    geno = []
+        return header
 
 
-
-    return geno
-
-def body_line_maker(ped_f, line):
-    """Return the body line to write in my otp"""
-    body_line = []
-
-    with open(ped_f, 'r') as my_ped :
-        my_ped_reader = csv.reader(my_ped, delimiter="\t")
-        count = 0
-
-        for rows in my_ped_reader :
-            count+=1
-            if count == line :
-                rows[1] + line_maker(rows[6:])
-                body_line.append(rows[1])
+    def piece(iterable, size, format=tuple):
+        """Sliding window of variable size"""
+        my_it = iter(iterable)
+        while True :
+            yield format(it.chain((next(my_it),), it.islice(my_it,size -1)))
 
 
+    def line_maker(seq):
+        """Return a list of allele format we want"""
+        geno = []
+        asso = ['AG','AG','AT','CG','CT','TG']
+
+        for allele in piece(seq,2) :
+            A1 = allele[0]
+            A2 = allele[1]
+            #possible cases
+            #missing data
+            if A1 == '0' and A2 == '0' :
+                geno.append('--')
+            #Hmz alleles
+            elif A1 == A2 :
+                geno.append(A1*2)
+            #Htz alleles
+            elif str(A1+A2) not in asso :
+                geno.append(A2+A1)
+            else :
+                geno.append(A1+A2)
+
+        return geno
 
 
-    return body_line
+    def body_line_maker(ped_f):
+        """Return the body line to write in my otp"""
+        body_line = []
+
+        with open(ped_f, 'r') as my_ped :
+            my_ped_reader = csv.reader(my_ped, delimiter=" ")
+
+            for rows in my_ped_reader :
+                indiv = []
+                indiv.append(rows[1])
+                indiv.extend(line_maker(rows[6:]))
+                body_line.append(indiv)
+
+        return body_line
 
 
 
-def write_output_file(ped_f, map_f, otp_f):
-    """Function that write the output file we want"""
-    header_line = header_line_maker(map_f)
-    body_line = []
+    def write_output_file(ped_f, map_f, otp_f):
+        """Function that write the output file we want"""
+        header_line = header_line_maker(map_f)
+        body_line = []
 
 
-    with open(otp_f, 'w') as my_otp :
-        my_otp_writer = csv.writer(my_otp, delimiter="\t")
+        with open(otp_f, 'w') as my_otp :
+            my_otp_writer = csv.writer(my_otp, delimiter="\t")
+
+            my_otp_writer.writerow(header_line_maker(map_f))
+            for indiv in body_line_maker(ped_f) :
+                my_otp_writer.writerow(indiv)
+
+
+
+
+
+
+    #__main__
+    write_output_file(ped_file, map_file, output)
