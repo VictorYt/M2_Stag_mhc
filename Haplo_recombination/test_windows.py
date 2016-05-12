c2e5b7685b21c01d4494ab9821230c1998b53940
VictorYt <victor.ythier@gmail.com>
2016-05-12 17:31:36 +0200

    try with KMP the search of recombination

--- Haplo_recombination/test_windows.py
+++ Haplo_recombination/test_windows.py
@@ -51,3 +51,37 @@ def read_input_file(filename, objecttype, delimit):
 		src.close()
 
 	return lst_of_objects
+
+
+
+
+def KnuthMorrisPratt(text, pattern):
+	"""Yields all starting positions of copies of the pattern in th text. 
+	Calling conventions are similar to string.find, but it's arguments can be lists
+	or iterators, not just strings, it returns all matches, no just the first one,
+	and it does not need the whole text in memory at once. 
+	Whenever it yields, it will have read the text exactly up to and
+	including the match that cused the yeild. 
+
+	"""
+	#allow indiexing into pattern and protect against change during yield
+	pattern = list(pattern) #attention je lui donnerai dejà une liste
+
+	#build table of shift amounts
+	shifts = [1] * (len(pattern) +1)
+	shift = 1
+	for pos in range(len(pattern)):
+		while shift <= pos and pattern[pos] != pattern[pos-shift]:
+			shift += shifts[pos-shift]
+		shifts[pos+1] = shift
+
+	#do the actual search
+	startPos = 0
+	matchLen = 0
+	for c in text :
+		while matchLen == len(pattern) or matchLen >= 0 and pattern[matchLen] != c :
+			startPos += shifts[matchLen]
+			matchLen -= shifts[matchLen]
+		matchLen += 1
+		if matchLen == len(pattern) :
+			yeild startPos
