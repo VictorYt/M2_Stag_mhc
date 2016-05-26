56e242ae12613e0f50c1464ed3feddd0a3c05b24
VictorYt <victor.ythier@gmail.com>
2016-05-26 15:58:51 +0200

    try a new output

--- Haplo_compare/Scripts/test.py
+++ Haplo_compare/Scripts/test.py
@@ -35,44 +35,75 @@ if __name__ == "__main__":
     threshold = arguments["--threshold"]
 
 
-    with open(haplotype_c_file, 'r') as my_haploC :
+    with open(haplotype_c_file, 'r') as my_haploC, open(output, 'w') as my_otp:
         my_haploC_reader = csv.reader(my_haploC, delimiter="\t")
+        my_otp_writer = csv.writer(my_otp, delimiter="\t")
 
         dico_SNP_contigue = {}
         header = True
+        
 
+        #parcour du fichier d'alignement ligne par ligne
         for rows in my_haploC_reader :
+            #traitement pendant la ligne d'en-tête
             if header :
-                #creation du dicotionaire ou je vais compter le nombre de fenêtre contigues
+                #creation du dicotionaire ou je vais ranger chaque portion de SNP contigues (id1, id2, startPos, endPos) commum en fonction de leur longeur (clef)
                 marker_size = len(rows[2:-1])
                 for x in range(marker_size+1) :
-                    dico_SNP_contigue[x] = 0
+                    #instanciation du dictionnaire par une liste vide.
+                    dico_SNP_contigue[x] = []
                 header = False
             else :
+                #create the future output line
+                line = [rows[0], rows[1]]
+                #séquence résultat de l'alignement des deux séquences rows[0] et rows[1]
                 pattern = rows[2:-1]
-                for match in pattern :
-                    if match == 0 :
-                        
+                #compteur des SNPs contigues
+                contigues = 0
+                #compteur de SNP (index)
+                count = 0
 
-                #remplir les valeur du dico now
+                #parcour de la séquence pattern SNP par SNP
+                for snp_allele in pattern :
+                    count +=1
 
-    pattern = list(pattern) #attention je lui donnerai dejà une liste
+                    #cas du 79ème SNP
+                    if count == len(pattern) :
+                        if snp_allele == "0" :
+                            if contigues == 0 :
+                                #create the fuure output line
+                                line = [rows[0], rows[1]]
+                                line.append(count)
+                                line.append(count)
+                                contigues += 1
+                                dico_SNP_contigue[contigues].append(line)
+                            else :
+                                contigues += 1
+                                line.append(count)
+                                dico_SNP_contigue[contigues].append(line)
 
-    #build table of shift amounts
-    shifts = [1] * (len(pattern) +1)
-    shift = 1
-    for pos in range(len(pattern)):
-        while shift <= pos and pattern[pos] != pattern[pos-shift]:
-            shift += shifts[pos-shift]
-        shifts[pos+1] = shift
 
-    #do the actual search
-    startPos = 0
-    matchLen = 0
-    for c in text :
-        while matchLen == len(pattern) or matchLen >= 0 and pattern[matchLen] != c :
-            startPos += shifts[matchLen]
-            matchLen -= shifts[matchLen]
-        matchLen += 1
-        if matchLen == len(pattern) :
-            yield (startPos)
+                    #cas du SNP contigue
+                    elif snp_allele == "0" :
+                        if contigues == 0 :
+                            #create the fuure output line
+                            line = [rows[0], rows[1]]
+                            line.append(count)
+                            contigues += 1
+                        else :
+                            contigues +=1
+
+                    #cas du SNP non contigue
+                    else :
+                        line.append(count-1)
+                        dico_SNP_contigue[contigues].append(line)
+                        contigues = 0
+                        #cas particulier de la clef 0 du dico_SNP_contigue. Y sera présent l'index du la endPos seulement
+
+
+
+        #écriture du fichier de sortie
+        for key, val in dico_SNP_contigue.items() :
+            for lst in val :
+                my_otp_writer.writerow(lst)
+            print(lst)
