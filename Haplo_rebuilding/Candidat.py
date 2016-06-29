680fe7d2f277a7dfbc96094e96236a0e59b11da5
VictorYt <victor.ythier@gmail.com>
2016-06-29 13:50:39 +0200

    add property to Candidat class

--- Haplo_rebuilding/Candidat.py
+++ Haplo_rebuilding/Candidat.py
@@ -22,10 +22,43 @@ class Candidat(Haplotype):
 		#Candidat specific to class
 		self._geno_ori = ""
 		self._haplo_ori = ""
-		self._index_htz_markers_in_seq = [] #liste des positions Htz dans seq du genotype (rend plus rapide la comparaison entre haplo possiblement combiné pour donner seq du génotype)
 		
 
 
 	def __str__(self):
 		"""Like str Haplotype a quick description of our Genotype object"""
 		return "Candidait {}, constructed using {} markers, is : {}".format(self._name, self._nbmarkers, self._sequence)
+
+	############
+	#ACCESSEURS#
+	############
+
+	def _get_geno_ori(self):
+		"""Return the attribut name of the Haplotype class"""
+		return self._geno_ori
+
+	def _get_haplo_ori(self):
+		"""Return the attribut sequence of the Haplotype class"""
+		return self._haplo_ori
+	
+	###########
+	#MUTATEURS#
+	###########
+
+	def _set_geno_ori (self, newGenoOri):
+		"""Change the name of our Haplotype object by a new one
+
+		Named parameters :
+		newname -- the new name selected (here first row of the input)
+		
+		"""
+		self._geno_ori = newGenoOri
+
+	def _set_haplo_ori(self, newHaploOri):
+		"""Change the sequence list of our Haplotype object by a new one
+
+		Named parameters :
+		newsequence -- the new sequence list selected (here the end of the row in our input)
+		
+		"""
+		self._haplo_ori = newHaploOri
